83327dc98eb048a02a8a4cf354c24b4344f66735
Joaquin Hojman <hojmanjoaquin@gmail.com>
2022-06-11 18:10:54 -0300

    conexion a database productiva

--- src/services/db.js
+++ src/services/db.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg')
-
+/*
 const pool = new Pool({
   user: 'postgres',
   database: 'wallets',
   password: '221096',
   port: 5432,
   host: 'localhost',
+})
+*/
+
+//databse productiva
+const pool = new Pool({
+  user: 'jolbhpluocrazi',
+  database: 'd75n4f1ldd4vd2',
+  password: 'fa908221e93758da916db844e67af351abd2476cb4417f7175895fdba23ab31d',
+  port: 5432,
+  host: 'ec2-52-204-195-41.compute-1.amazonaws.com',
 })
 
 module.exports = { pool };
