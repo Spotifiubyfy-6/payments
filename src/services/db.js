4a056953d60ca2d0545c0e119e074e2966a70674
Joaquin Hojman <hojmanjoaquin@gmail.com>
2022-06-13 15:15:01 -0300

    problema con database productiva solucionado

--- src/services/db.js
+++ src/services/db.js
@@ -9,13 +9,13 @@ const pool = new Pool({
 })
 */
 
-//databse productiva
+//database productiva
 const pool = new Pool({
   user: 'jolbhpluocrazi',
-  database: 'd75n4f1ldd4vd2',
   password: 'fa908221e93758da916db844e67af351abd2476cb4417f7175895fdba23ab31d',
+  database: 'd75n4f1ldd4vd2',
   port: 5432,
   host: 'ec2-52-204-195-41.compute-1.amazonaws.com',
+  ssl: { rejectUnauthorized: false }
 })
-
 module.exports = { pool };
